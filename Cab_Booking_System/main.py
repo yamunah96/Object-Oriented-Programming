fa4a67ec02ca629b5452fc18193a257e968e60ee
Yamuna H <70136290+yamunah96@users.noreply.github.com>
2026-08-14 16:27:34 +0530

    Refactor Vehicle class and booking process
    Refactored Vehicle class and improved ride booking logic.

--- Cab_Booking_System/main.py
+++ Cab_Booking_System/main.py
@@ -1,48 +1,71 @@
+# parent class
 class Vehicle:
-    #  common info
-    def __init__(self,vehicle_no:str=None,brand_name:str=None, driver_name:str=None,distance:float=0):
-        self.vehicle_no= vehicle_no
-        self.brand_name= brand_name
-        self.driver_name= driver_name
-        self.distance= distance
-
-    # calcualte total travel fare
-    def calculate_fare(self):
-        return self.distance*self.price_per_km
+    def __init__(self,driver_name:str=None,vehicle_no:str=None,brand_name:str=None,price_per_km:float=0):
+        # instance(object) attributes
+        self.driver_name=driver_name
+        self.vehicle_no=vehicle_no
+        self.brand_name=brand_name
+        self.price_per_km=price_per_km
 
     def display(self):
-        print("Your Cab has booked successfully.. Enjoy the ride")
-        print(f"Driver:{self.driver_name}\nDistance:{self.distance}KM\nRate:₹{self.price_per_km}/KM\nTotal Fare: ₹{self.calculate_fare()}")
+        return f"Driver: {self.driver_name}\nVehicle:{self.brand_name}\nVehicle_no:{self.vehicle_no}\nPrice Per KM: ₹{self.price_per_km}/KM"
 
-#  child class -- Car
+# Child class car
 class Car(Vehicle):
-    price_per_km=10
+    pass
 
-#  child class -- Bike
+# Child class bike
 class Bike(Vehicle):
-    price_per_km=5
-try:
-    vehicle_type= input("Select the vehicle Car/Bike: ").lower().strip()
+    pass
 
-    if vehicle_type == "car":
-        vehicle_no= input("Enter the vehicle no: ").lower().strip()
-        driver= input("Enter the driver name: ").lower().strip()
-        brand_name= input("Enter car brand name: ").lower().strip()
-        distance= float(input("Enter travel distance in KM: "))
-        vehicle= Car(vehicle_no,brand_name,driver,distance)
-    elif vehicle_type == "bike":
-        vehicle_no= input("Enter the vehicle no: ").lower().strip()
-        driver= input("Enter the driver name: ").lower().strip()
-        brand_name= input("Enter car brand name: ").lower().strip()
-        distance= float(input("Enter travel distance in KM: "))
-        vehicle= Bike(vehicle_no,brand_name,driver,distance)
+car1=Car("Rahul","KA1234","Toyota",8)
+car2= Car("Yash","KA2626","Maruti Suzki",6)
+
+bike1=Bike("Rina","KA7680","Tvs",5)
+bike2= Bike("Gagan","KA9090","Active Honda",6)
+
+vehicle_avilable={
+    "car":[car1,car2],
+    "bike":[bike1,bike2]
+}
+while True:
+    vehicle_type= input("enter vehicle type Car/Bike: ").lower().strip()
+    if vehicle_type in vehicle_avilable:
+        print("="*70)
+        for data in vehicle_avilable[vehicle_type]:
+            print(data.display())
+            print("="*70)
+        vehicle_no= input("enter vehicle_no to book a ride: ").lower().strip()
+        selected_vehicle=None
+
+        for data in vehicle_avilable[vehicle_type]:
+            if vehicle_no == data.vehicle_no.lower():
+                selected_vehicle=data
+
+        if selected_vehicle==None:
+            print("Wrong vehicle no, try again")
+            continue
+
+        
+        try:
+            distance= float(input("Enter the travel distance in km: "))
+            if distance <=0:
+                print("distance must be greater than zero..")
+                continue
+        except ValueError as e:
+            print(e)
+            continue
+        
+        total= distance* selected_vehicle.price_per_km
+
+        print("Your Ride Booke successfully")
+        print("="*60)
+        print( f"Driver: {selected_vehicle.driver_name}\nVehicle:{selected_vehicle.brand_name}\nVehicle_no:{selected_vehicle.vehicle_no}\nPrice Per KM: ₹{selected_vehicle.price_per_km}/KM\nTotal Fare: ₹{total}")
+        print("="*60)
+        answer= input("Would you like to book one more trip yes/no: ")
+        if answer!="yes":
+            print("Thanks for booking an ride")
+            break
     else:
-        print("enter the valid vehicle_type")
-
-    if vehicle:
-        vehicle.display()
-    else:
-        print("Unable to book the ride")
-
-except Exception as e:
-    print(e)
+        print("vehicle is not available")
+        break
